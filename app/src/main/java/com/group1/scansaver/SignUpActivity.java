5140fa68978ce6e2e8a3c461c5987a53ae914ff7
JulianoF <juliano1@live.ca>
2024-10-25 12:20:27 -0400

    Work on Login UI

--- app/src/main/java/com/group1/scansaver/SignUpActivity.java
+++ app/src/main/java/com/group1/scansaver/SignUpActivity.java
@@ -1,40 +1,24 @@
 package com.group1.scansaver;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onClickForgot(View v){
-        Toast.makeText(this, "You forgot PAssword Clicked",
-                Toast.LENGTH_LONG).show();
-    }
-
-    public void onClickCreateNew(View v){
-        Toast.makeText(this, "Create New Account CLicked",
-                Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
     }
 }
